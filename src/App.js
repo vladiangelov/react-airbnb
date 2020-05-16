9249437245af6b8db1a8784f8a0c83c1e2615220
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-05-16 11:15:50 +0100

    Adding google maps react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './App.css';
 import FlatList from './components/FlatList';
 import flats from './data/flats';
-import SimpleMap from './components/SimpleMap';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div style={{color: "red"}}>O</div>;
 
 function App() {
 
   const flats_imported = flats;
+
+  const center = () => {
+    return {
+      lat: 51.12,
+      lng: -0.159
+    };
+  }
+
 
   return (
     <div className="App">
@@ -14,7 +24,9 @@ function App() {
         <FlatList flats={flats_imported} />
       </div>
       <div className="right-scene">
-
+        <GoogleMapReact defaultCenter={center()} defaultZoom={15} bootstrapURLKeys={{ key: process.env.REACT_APP_APIKEY }}>
+          <AnyReactComponent />
+        </GoogleMapReact>
       </div>
     </div>
   );
